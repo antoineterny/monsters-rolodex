14af5f258b2fefd9913180a6455a31d20d29967b
Antoine Terny <antoine.terny@gmail.com>
2020-12-20 23:44:20 +0100

    adding files for github pages

---
+++ src/components/card-list/card-list.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+import {Card} from '../card/card'
+
+import "./card-list.css"
+
+export const CardList = props => (
+	<div className="card-list">
+		{props.monsters.map(monster => (
+			<Card key={monster.id} monster={monster}/>
+		))}
+	</div>
+)
